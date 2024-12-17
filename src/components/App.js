2bf8e504c836f93f3e4a30d0d5ad5b35e9416cc0
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2024-12-18 00:18:21 +0530

    button work

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React, {useState}from "react";
+
 import './../styles/App.css';
 
+
 const App = () => {
+
+  const[count, setcount] = useState(0)
+  const increasebyone = () => {
+    setcount(count + 1)
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {count} times.</p>
+        <button onClick={increasebyone}>Click me</button>
     </div>
   )
 }
