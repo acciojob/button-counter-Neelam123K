be80b69d839b089c48ca8dd11f9f2db8036d9c5a
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-01-08 11:15:03 +0530

    enter your name

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,24 @@
+import React, { useState } from "react";
 
-import React, {useState}from "react";
-
-import './../styles/App.css';
-
+import "./../styles/App.css";
 
 const App = () => {
-
-  const[count, setcount] = useState(0)
-  const increasebyone = () => {
-    setcount(count + 1)
-  }
+  const [count, setCount] = useState(0); 
 
   return (
     <div>
-        {/* Do not remove the main div */}
-        <p>Button clicked {count} times.</p>
-        <button onClick={increasebyone}>Click me</button>
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
+      <div>{count}</div>
 
-export default App
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </button>
+    </div>
+  );
+};
+
+export default App;
